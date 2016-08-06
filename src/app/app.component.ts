a2d4442efcfc845f0976225941e5d2db04b20929
Michael Dausmann <mdausmann@gmail.com>
2016-08-06 17:31:46 +1000

    attempt to introduce d3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { ScatterComponent } from './scatter/scatter.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  styleUrls: ['app.component.css']
+  styleUrls: ['app.component.css'],
+  directives: [ ScatterComponent ]
 })
 export class AppComponent {
   title = 'app works!';
---
+++ src/app/scatter/scatter.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+import * as d3 from 'd3';
+
+@Component({
+  moduleId: module.id,
+  selector: 'my-scatter',
+  template: `<ng-content></ng-content><p>scatter here</p>`
+})
+export class ScatterComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit() { }
+
+}
